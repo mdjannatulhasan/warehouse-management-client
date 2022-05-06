f0428ad7601b29f366df77a90db68adfbdcb8778
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-05-06 18:27:58 +0600

    Navbar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,19 +9,24 @@ const Header = () => {
     const [user, loading, error] = useAuthState(auth);
 
     return (
-        <div>
-            <nav className="space-x-5">
-                <Link to="/">Home</Link>
-                <Link to="/inventory">Inventory</Link>
-                {user?.uid ? (
-                    <button onClick={() => signOut(auth)}>Sign Out</button>
-                ) : (
-                    <>
-                        <Link to="/register">Register</Link>
-                        <Link to="/login">Login</Link>
-                    </>
-                )}
-            </nav>
+        <div className="bg-[#a5b8cc] py-2">
+            <div className="container flex justify-between items-center">
+                <div>
+                    <img width="230px" src="images/site-logo.png" alt="" />
+                </div>
+                <nav className="space-x-5 text-lg">
+                    <Link to="/">Home</Link>
+                    <Link to="/inventory">Inventory</Link>
+                    {user?.uid ? (
+                        <button onClick={() => signOut(auth)}>Sign Out</button>
+                    ) : (
+                        <>
+                            <Link to="/register">Register</Link>
+                            <Link to="/login">Login</Link>
+                        </>
+                    )}
+                </nav>
+            </div>
         </div>
     );
 };
